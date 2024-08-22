c69cab58becc0b9ffc37741f7c3f9236cfd8a5e2
sagiv <sagivram15193@gmail.com>
2024-08-22 14:24:03 +0300

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by sagiv on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
